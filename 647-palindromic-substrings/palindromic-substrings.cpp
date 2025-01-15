94c96a408d5746dee116c21fc6c364dd3c92d35e
HardikDDeore12 <124691435+HardikDDeore12@users.noreply.github.com>
2025-01-15 13:49:49 +0530

    Time: 30 ms (28.57%) | Memory: 11.2 MB (52.54%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-// memoization
-int t[1001][1001];
-    bool check(string& s, int i,int j){
-        if(i>j){
-            return true;
-        }
-
-        if(t[i][j] != -1){
-            return t[i][j];
-        }
-
-        if(s[i] == s[j]){
-            return t[i][j] = check(s,i+1,j-1);
-        }
-
-        return false;
-    }
     int countSubstrings(string s) {
         int n = s.length();
 
-        memset(t,-1,sizeof(t));
+        vector<vector<bool>> t(n, vector<bool>(n, false));
+        // STATE -- t[i][j]=true :s[i:j] is a palindromic substring where i and j are inclusive 
 
-        int count = 0;
+        int count  = 0;
 
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(check(s,i,j)){
+        for(int L=1;L<=n; L++){
+            for(int i=0;i+L-1<n ; i++){
+                int j = i+L-1;
+
+                if(i==j){
+                    t[i][j] = true;
+                }else if(i+1 == j){
+                    t[i][j] = (s[i] == s[j]);
+                }else{
+                    t[i][j] = (s[i] == s[j]  &&  t[i+1][j-1]);
+                }
+
+                if(t[i][j] == true){
                     count++;
                 }
             }
+                
         }
-
         return count;
     }
 };
