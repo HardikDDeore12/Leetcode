d5c87631ca76e3ec0552e04dd5d191eb0909de4f
HardikDDeore12 <124691435+HardikDDeore12@users.noreply.github.com>
2025-01-18 19:55:56 +0530

    Time: 435 ms (73.57%) | Memory: 154.7 MB (46.58%) - LeetSync

---
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int numSubmatrixSumTarget(std::vector<std::vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        // cumulative sum row wise
+        for (int row = 0; row < m; row++) {
+            for (int col = 1; col < n; col++) {
+                matrix[row][col] += matrix[row][col - 1];
+            }
+        }
+
+        int count = 0;
+        // now find the no of subarrays having sum == target (leetcode-560)
+        for (int startCols = 0; startCols < n; startCols++) {
+            for (int j = startCols; j < n; j++) {
+                unordered_map<int, int> mp;
+                mp.insert({0,1});
+                int Cum_sum = 0;
+
+                for (int row = 0; row < m; row++) {
+                    Cum_sum += matrix[row][j] - (startCols > 0 ? matrix[row][startCols - 1] : 0);
+
+                    if(mp.find(Cum_sum - target) != mp.end()){
+                        count += mp[Cum_sum - target];
+                    }
+                
+                    mp[Cum_sum]++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
+
+
+
