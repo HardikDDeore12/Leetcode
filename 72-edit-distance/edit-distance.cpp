5bf4f65cb54baee0790415bd44f59cf6de755b78
HardikDDeore12 <124691435+HardikDDeore12@users.noreply.github.com>
2025-01-20 14:34:49 +0530

    Time: 47 ms (17.57%) | Memory: 11.7 MB (84.96%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     int t[501][501];
-    int solve(string& s1, string& s2, int i, int j){
-        int m = s1.length();
-        int n = s2.length();
-
-        if(t[i][j] != -1){
-            return t[i][j];
+    int solve(string& s1, string& s2, int m, int n){
+        if(t[m][n] != -1){
+            return t[m][n];
         }
 
-        if(i == m){
-            return n-j; //insertions
-        }else if(j == n){
-            return m-i; //deletions
+        if(m==0){
+            return n; //insertions
+        }else if(n==0){
+            return m; //deletions
         }
 
-        if(s1[i] == s2[j]){
-            return t[i][j] = solve(s1,s2,i+1,j+1);
+        if(s1[m-1] == s2[n-1]){
+            return t[m][n] = solve(s1,s2,m-1,n-1);
         }
 
-        int Insert = 1 + solve(s1,s2,i,j+1);
-        int Delete = 1 + solve(s1,s2,i+1,j);
-        int Replace = 1 + solve(s1,s2,i+1,j+1);
+        int Insert = 1 + solve(s1,s2,m,n-1);
+        int Delete = 1 + solve(s1,s2,m-1,n);
+        int Replace = 1 + solve(s1,s2,m-1,n-1);
 
-        return t[i][j] = min({Insert, Delete, Replace});
+        return t[m][n] = min({Insert, Delete, Replace});
     }
     int minDistance(string s1, string s2) {
+        int m = s1.length();
+        int n = s2.length();
         memset(t,-1,sizeof(t));
-        return solve(s1,s2,0,0);
+        return solve(s1,s2,m,n);
     }
     
 };
