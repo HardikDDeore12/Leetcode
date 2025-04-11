08078da0a090ca5e799eda304bfedbbf0ae9769d
HardikDDeore12 <124691435+HardikDDeore12@users.noreply.github.com>
2025-04-12 02:19:37 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (32.91%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -5,31 +5,28 @@ public:
 
         int gola_index = -1;
 
-        //the largest index where nums[i-1] < nums[i]
-        for (int i=n-1;i>0;i--) {
-            if (nums[i-1] < nums[i]) {
+        for(int i=n-1;i>0;i--){
+            if(nums[i-1]<nums[i]){
                 gola_index = i-1;
                 break;
             }
         }
 
-        // If no such index is found, reverse the entire array
-        if (gola_index == -1) {
-            reverse(nums.begin(), nums.end());
+        if(gola_index == -1){
+            reverse(nums.begin(),nums.end());
             return;
         }
 
-        //largest index greater than gola_index where nums[i] > nums[gola_index]
         int swap_index = gola_index;
-        for (int i = n - 1; i > gola_index; i--) {
-            if (nums[i] > nums[gola_index]) {
+        for(int i=n-1;i> gola_index;i--){
+            if(nums[i]>nums[gola_index]){
                 swap_index = i;
                 break;
             }
         }
 
-        swap(nums[gola_index], nums[swap_index]);
+        swap(nums[gola_index],nums[swap_index]);
 
-        reverse(nums.begin() + gola_index + 1, nums.end());
+        reverse(nums.begin() + gola_index + 1,nums.end());
     }
 };
